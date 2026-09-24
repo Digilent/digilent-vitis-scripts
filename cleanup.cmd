44154ac81da73196b613cf749c5cfa35d6bf5e83
bog739 <bogdan.stanese@digilent.ro>
2026-09-24 16:21:00 +0300

    Preserve Git metadata during repo cleanup

--- cleanup.cmd
+++ cleanup.cmd
@@ -1,12 +1,22 @@
 
 @echo off
+setlocal enabledelayedexpansion
 rem save current working directory
 pushd %~dp0
 
-rem delete all files from subfolders
-for /d /r %%i in (*) do del /f /q %%i\*
-rem delete all subfolders
-for /d %%i in (*) do rd /S /Q %%i
+rem delete all files from subfolders, but never touch Git metadata (.git can
+rem be a directory in a standalone clone, or a submodule config file). Uses
+rem a delayed-expansion substring check instead of piping to findstr, since
+rem a pipe inside a parenthesized for/do block is unreliable in batch files.
+for /d /r %%i in (*) do (
+	set "p=%%i"
+	set "chk=!p:.git=!"
+	if "!p!"=="!chk!" del /f /q "%%i\*"
+)
+rem delete all subfolders except .git
+for /d %%i in (*) do (
+	if /I not "%%~nxi"==".git" rd /S /Q "%%i"
+)
 
 rem unmark read only from all files
 attrib -R .\* /S
@@ -23,6 +33,7 @@ attrib +R .\_vitis.sh
 attrib +R .\LICENSE
 attrib +R .\README.md
 attrib +R .\.gitignore
+attrib +R .\.git
 
 rem delete all non read-only
 del /Q /A:-R .\*
@@ -31,4 +42,5 @@ rem unmark read-only
 attrib -R .\*
 
 rem restore original working directory
+endlocal
 popd
--- cleanup.sh
+++ cleanup.sh
@@ -9,11 +9,13 @@
 
 script_dir=$(dirname ${BASH_SOURCE[0]})
 
-# Remove directories/subdirectories
-find $script_dir -mindepth 1 -type d -exec rm -rf {} +
+# Remove directories/subdirectories, but never Git metadata (.git can be a
+# directory in a standalone clone or a file in a submodule; either way, do
+# not let it be swept up as a generic "other file" below).
+find $script_dir -mindepth 1 -name '.git' -prune -o -type d -exec rm -rf {} +
 
 # Remove any other files than:
-find $script_dir -type f ! -name 'cleanup.sh'  \
+find $script_dir -name '.git' -prune -o -type f ! -name 'cleanup.sh'  \
 			 ! -name 'cleanup.cmd' \
 			 ! -name 'checkin.py'  \
 			 ! -name 'checkout.py' \
